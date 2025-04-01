6b18945d4aea0061bd7e2c316556eb3698ff6981
Dhahinya123 <dhahinyarumugam@gmail.com>
2025-04-01 22:30:51 +0530

    Create Event.java

---
+++ Event.java
@@ -0,0 +1,72 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import jakarta.persistence.*;
+
+@Entity
+public class Event {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-generate IDs
+    private Long id;
+
+    private String eventname;
+    private LocalDate date;
+    private LocalTime time;
+    private int seats;
+
+    // Many-to-One mapping with User
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEventname() {
+        return eventname;
+    }
+
+    public void setEventname(String eventname) {
+        this.eventname = eventname;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
